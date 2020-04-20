6e7d54f524d22af798c27e76034023209b3b4886
Annie Huang <annie.hn@gmail.com>
2020-04-20 21:54:57 +1000

    Create YoutubeForm.js component and add it into App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import YoutubeForm from "./components/YoutubeForm";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <YoutubeForm />
+        </div>
+    );
 }
 
 export default App;
