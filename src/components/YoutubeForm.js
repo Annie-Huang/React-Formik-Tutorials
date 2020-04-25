e66cc9ba02701ae42b2d5bd1452cc0fba5ee6248
Annie Huang <annie.hn@gmail.com>
2020-04-25 15:35:50 +1000

    Hook each input's onChange method and value properties, and you can see while you typing the input field, you can see it displaying in console.

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -12,17 +12,19 @@ const YoutubeForm = () => {
         }
     });
 
+    console.log('Form values', formik.values);
+
     return (
         <div>
             <form>
                 <label htmlFor='name'>Name</label>
-                <input type="text" id='name' name='name' />
+                <input type="text" id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
 
                 <label htmlFor='email'>E-mail</label>
-                <input type="text" id='email' name='email' />
+                <input type="text" id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
 
-                <label htmlFor='channel'>Name</label>
-                <input type="text" id='channel' name='channel' />
+                <label htmlFor='channel'>Channel</label>
+                <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.chanel}/>
 
                 <button>Submit</button>
             </form>
