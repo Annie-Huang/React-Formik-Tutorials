638d2165be786193a9e8a3804f876cd2e6aa668c
Annie Huang <annie.hn@gmail.com>
2020-05-21 22:40:36 +1000

    Display error msg with the render prop pattern

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -235,7 +235,12 @@ const YoutubeForm = () => {
                 <div className="form-control">
                     <label htmlFor='email'>E-mail</label>
                     <Field type="text" id='email' name='email' />
-                    <ErrorMessage name='email' />
+                    {/*<ErrorMessage name='email' />*/}
+                    <ErrorMessage name='email'>
+                        {
+                            (errorMsg) => <div className='error'>{errorMsg}</div>
+                        }
+                    </ErrorMessage>
                 </div>
 
                 <div className="form-control">
