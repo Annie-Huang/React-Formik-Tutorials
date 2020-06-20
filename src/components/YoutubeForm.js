c27c0381a55a240059960be9160fb68f9f0b1586
Annie Huang <annie.hn@gmail.com>
2020-06-20 21:06:16 +1000

    Field Level Validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -22,20 +22,131 @@ const onSubmit = values => {
 }
 
 const validationSchema = Yup.object({
-    name: Yup.string().required('Required!!!'), // Use a differnt string from validate
+    name: Yup.string().required('Required!!!'), // Use a different string from validate
     email: Yup.string().email('Invalid email format').required('Required!!!'),
     channel: Yup.string().required('Required!!!')
 });
 
-/*
-form.errors get populated when
-1. A change event has occurred (e.g. typing in something in the channel field.)         << can change through validateOnChange props
-2. After any blur event in the form (e.g click into the channle field and click out)    << can change through validateOnBlur props
-3. Clicks on the submit button
-*/
+// You will use this, e.g. when you want to render your fields based on a json that you fetch from an api call
+// Once you get the json representation of your form you iterate over the different objects building the validation function in each
+// iteration and assigning it to the validate prop on the field or the fast field component
+const validateComments = value => {
+    let error;
+    if(!value) {
+        error = 'Required';
+    }
+    return error;
+}
+
+// /*
+// form.errors get populated when
+// 1. A change event has occurred (e.g. typing in something in the channel field.)         << can change through validateOnChange props
+// 2. After any blur event in the form (e.g click into the channle field and click out)    << can change through validateOnBlur props
+// 3. Clicks on the submit button
+// */
+// const YoutubeForm = () => {
+//     return (
+//         <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} validateOnChange={false} validateOnBlur={false}>
+//             <Form>
+//                 <div className="form-control">
+//                     <label htmlFor='name'>Name</label>
+//                     <Field type="text" id='name' name='name' />
+//                     <ErrorMessage name='name' component={TextError} />
+//                 </div>
+//
+//                 <div className="form-control">
+//                     <label htmlFor='email'>E-mail</label>
+//                     <Field type="text" id='email' name='email' />
+//                     <ErrorMessage name='email'>
+//                         {
+//                             (errorMsg) => <div className='error'>{errorMsg}</div>
+//                         }
+//                     </ErrorMessage>
+//                 </div>
+//
+//                 <div className="form-control">
+//                     <label htmlFor='channel'>Channel</label>
+//                     <Field type="text" id='channel' name='channel' placeholder='Youtube channel name' />
+//                     <ErrorMessage name='channel' />
+//                 </div>
+//
+//                 <div className="form-control">
+//                     <label htmlFor='comments'>Comments</label>
+//                     <Field as='textarea' id='comments' name='comments' />
+//                 </div>
+//
+//                 <div className="form-control">
+//                     <label htmlFor='address'>Address</label>
+//
+//                     <FastField name='address'>
+//                         {
+//                             // If you update channel, the address will not keep re-rendering if you use FastField, it implement shouldComponentUpdate internally
+//                             (props) => {
+//                                 console.log('Render props', props);
+//                                 const {field, form, meta} = props;
+//                                 return (
+//                                     <div>
+//                                         <input type='text' id='address' {...field}/>
+//                                         {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+//                                     </div>
+//                                 )
+//                             }
+//                         }
+//                     </FastField>
+//                 </div>
+//
+//                 <div className="form-control">
+//                     <label htmlFor='facebook'>Facebook profile</label>
+//                     <Field type='text' id='facebook' name='social.facebook' />
+//                 </div>
+//                 <div className="form-control">
+//                     <label htmlFor='twitter'>Twitter profile</label>
+//                     <Field type='text' id='twitter' name='social.twitter' />
+//                 </div>
+//
+//                 <div className='form-control'>
+//                     <label htmlFor='primaryPh'>Primary phone number</label>
+//                     <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+//                 </div>
+//                 <div className='form-control'>
+//                     <label htmlFor='secondaryPh'>Secondary phone number</label>
+//                     <Field type='text' id='secondaryPh' name='phoneNumbers[1]' />
+//                 </div>
+//
+//                 <div className='form-control'>
+//                     <label>List of phone numbers</label>
+//                     <FieldArray name='phNumbers'>
+//                         {
+//                             (fieldArrayProps) => {
+//                                 // console.log('fieldArrayProps', fieldArrayProps); // you can check other methods available in fieldArrayProps
+//                                 const {push, remove, form} = fieldArrayProps;
+//                                 const {values} = form;
+//                                 const {phNumbers} = values;
+//                                 console.log('Form errors', form.errors);
+//                                 return <div>
+//                                     {
+//                                         phNumbers.map((phNumber, index) => (
+//                                             <div key={index}>
+//                                                 <Field name={`phNumbers[${index}]`} />
+//                                                 {index > 0 && <button type='button' onClick={() => remove(index)}> - </button>}
+//                                                 <button type='button' onClick={() => push('')}> + </button>
+//                                             </div>
+//                                         ))
+//                                     }
+//                                 </div>
+//                             }
+//                         }
+//                     </FieldArray>
+//                 </div>
+//
+//                 <button type='submit'>Submit</button>
+//             </Form>
+//         </Formik>
+//     );
+// };
 const YoutubeForm = () => {
     return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} validateOnChange={false} validateOnBlur={false}>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
             <Form>
                 <div className="form-control">
                     <label htmlFor='name'>Name</label>
@@ -61,7 +172,8 @@ const YoutubeForm = () => {
 
                 <div className="form-control">
                     <label htmlFor='comments'>Comments</label>
-                    <Field as='textarea' id='comments' name='comments' />
+                    <Field as='textarea' id='comments' name='comments' validate={validateComments}/>
+                    <ErrorMessage name='comments' component={TextError}/>
                 </div>
 
                 <div className="form-control">
