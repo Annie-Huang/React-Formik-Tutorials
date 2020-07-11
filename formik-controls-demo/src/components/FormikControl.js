10143a531fb17eb965c93b440d9852013bfa0628
Annie Huang <annie.hn@gmail.com>
2020-07-11 18:47:37 +1000

    React Formik Tutorial - 34 - Textarea

--- formik-controls-demo/src/components/FormikControl.js
+++ formik-controls-demo/src/components/FormikControl.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Input from "./Input";
+import Textarea from "./Textarea";
 
 const FormikControl = ({control, ...rest}) => {
     switch (control) {
         case 'input':
             return <Input {...rest} />;
         case 'textarea':
+            return <Textarea {...rest} />;
         case 'select':
         case 'radio':
         case 'checkbox':
--- formik-controls-demo/src/components/Textarea.js
+++ formik-controls-demo/src/components/Textarea.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Field, ErrorMessage} from 'formik';
 import TextError from "./TextError";
 
+/*
+  Need to pass: control = 'textarea', label = 'Description', name = 'description'
+ */
 const Textarea = ({label, name, ...rest}) => (
     <div className='form-control'>
         <label htmlFor={name}>{label}</label>
