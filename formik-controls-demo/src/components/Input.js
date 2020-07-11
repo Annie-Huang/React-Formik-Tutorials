40b9978391c326c38595bb8249fedd3f7164a89b
Annie Huang <annie.hn@gmail.com>
2020-07-11 17:57:26 +1000

    Create TextError.js and Input.js component

---
+++ formik-controls-demo/src/components/Input.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Field, ErrorMessage} from 'formik';
+import TextError from "./TextError";
+
+/*
+  Need to pass: control = 'input', label = 'Email', name = 'email', type = 'email'
+ */
+const Input = ({label, name, ...rest}) => {
+    return (
+        <div className='form-control'>
+            <label htmlFor={name}>{label}</label>
+            <Field id={name} name={name} {...rest} />
+            <ErrorMessage name={name} component={TextError}/>
+        </div>
+    );
+};
+
+export default Input;
