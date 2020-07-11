1c9c4f22cac37668305531cea18d7b6652d5adb9
Annie Huang <annie.hn@gmail.com>
2020-07-11 19:03:17 +1000

    Created Select.js component

---
+++ formik-controls-demo/src/components/Select.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Field, ErrorMessage} from 'formik';
+import TextError from "./TextError";
+
+/*
+  Need to pass: control = 'select', label = 'Select a topic', name = 'selectOption', options = [{key, value}]
+ */
+const Select = ({label, name, options, ...rest}) => (
+    <div className='form-control'>
+        <label htmlFor={name}>{label}</label>
+        <Field as='select' id={name} name={name} {...rest}>
+            {
+                options.map(option => <option key={option.value} value={option.value}>{option.key}</option>)
+            }
+        </Field>
+        <ErrorMessage name={name} component={TextError}/>
+    </div>
+);
+
+export default Select;
