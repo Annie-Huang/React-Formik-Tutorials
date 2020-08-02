a13cd2a6890ad909716d249782989255d6b8d44e
Annie Huang <annie.hn@gmail.com>
2020-08-02 22:01:37 +1000

    Call react-datepicker from DatePicker.js

--- formik-controls-demo/src/components/DatePicker.js
+++ formik-controls-demo/src/components/DatePicker.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import {Field} from "formik";
+import {ErrorMessage, Field} from "formik";
+import DateView from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import TextError from "./TextError";
+
 
 const DatePicker = ({label, name, ...rest}) => {
+    /*
+() => {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+  );
+};
+ */
+
     return (
         <div className='form-control'>
             <label htmlFor={name}>{label}</label>
@@ -10,9 +23,19 @@ const DatePicker = ({label, name, ...rest}) => {
                     ({form, field}) => {
                         const {setFieldValue} = form;
                         const {value} = field;
+
+                        return (
+                            <DateView
+                                id={name}
+                                {...field}
+                                {...rest}
+                                selected={value}
+                                onChange={val => setFieldValue(name, val)}
+                            />)
                     }
                 }
             </Field>
+            <ErrorMessage name={name} component={TextError}/>
         </div>
     );
 };
