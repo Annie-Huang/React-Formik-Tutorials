ef76fe5de25b9ccd1d40a3a176c58f965e086dcf
Annie Huang <annie.hn@gmail.com>
2020-08-07 21:17:40 +1000

    React Formik Tutorial - 42 - Wiring up a UI Library

--- formik-controls-demo/src/App.js
+++ formik-controls-demo/src/App.js
@@ -4,15 +4,19 @@ import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
 import RegistrationForm from "./components/RegistrationForm";
 import EnrollmentForm from "./components/EnrollmentForm";
+import {theme , ThemeProvider} from '@chakra-ui/core'
+
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm/>
-      <RegistrationForm />
-      <EnrollmentForm />
-      <FormikContainer/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <LoginForm/>
+        <RegistrationForm />
+        <EnrollmentForm />
+        <FormikContainer/>
+      </div>
+    </ThemeProvider>
   );
 }
 
