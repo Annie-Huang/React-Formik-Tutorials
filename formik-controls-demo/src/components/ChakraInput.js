6cab4834f2037093677e6e75d9d2c3e108234328
Annie Huang <annie.hn@gmail.com>
2020-08-08 11:50:03 +1000

    Add validation rules

--- formik-controls-demo/src/components/ChakraInput.js
+++ formik-controls-demo/src/components/ChakraInput.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Field} from "formik";
-import FormikControl from "./FormikControl";
-import {FormErrorMessage, FormLabel} from "@chakra-ui/core";
+import {Input, FormControl, FormErrorMessage, FormLabel} from "@chakra-ui/core";
 
+// The isInvalid property in FormControl applies two thing.
+// 1. The entire FormControl will have a red border if the field state is invalid.
+// 2. It controls the display of the form error message component.
+// Compare this with Input.js
 const ChakraInput = ({label, name, ...rest}) => {
   return (
     <Field name={name}>
       {
         ({field, form}) => {
-          return <FormikControl>
+          return <FormControl isInvalid={form.errors[name] && form.touched[name]}>
             <FormLabel htmlFor={name}>{label}</FormLabel>
-            <input id={name} {...rest} {...field}/>
+            <Input id={name} {...rest} {...field}/>
             <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
-          </FormikControl>
+          </FormControl>
         }
       }
 
